31525ae1a554129431ec83898df0e02fb8124bcf
Smiley <adamtreysmiley@gmail.com>
2026-02-28 12:25:55 -0600

    Make single seq, update ignore

--- get_mmseqs2_msa.py
+++ get_mmseqs2_msa.py
@@ -4,40 +4,28 @@ import requests
 import time
 import random
 import tarfile
-from typing import Sequence, Dict, Tuple, Union
+from typing import Dict
 from Bio import SeqIO
-
-
-def list_files_with_extensions(dir, extensions):
-    return [f for f in os.listdir(dir) if f.endswith(extensions)]
 
 
 def runMMseqs2(
         out_path: str,
-        sequences: Union[Sequence[str], str],
-        use_env: bool = True,
+        sequence: str,
         use_filter: bool = True,
-        use_pairing: bool = False,
         host_url: str = 'https://api.colabfold.com'
-        ) -> Sequence[str]:
-    """ Computes MSAs by querying MMseqs2 API. """
+        ) -> str:
+    """ Computes an MSA for a single sequence by querying MMseqs2 API.
+    Always searches environmental databases (env). """
 
-    submission_endpoint = 'ticket/pair' if use_pairing else 'ticket/msa'
-
-    def submit(seqs: Sequence[str], mode: str, N=101) -> Dict[str, str]:
-        """ Submits a query of sequences to MMseqs2 API. """
-        n, query = N, ''
-        for seq in seqs:
-            query += f'>{n}\n{seq}\n'
-            n += 1
-
-        res = requests.post(f'{host_url}/{submission_endpoint}',
+    def submit(seq: str, mode: str, N: int = 101) -> Dict[str, str]:
+        """ Submits a query sequence to MMseqs2 API. """
+        query = f'>{N}\n{seq}\n'
+        res = requests.post(f'{host_url}/ticket/msa',
                             data={'q': query, 'mode': mode}, verify=False)
         try:
             out = res.json()
         except ValueError:
             out = {'status': 'ERROR'}
-
         return out
 
     def status(ID: int) -> Dict[str, str]:
@@ -47,7 +35,6 @@ def runMMseqs2(
             out = res.json()
         except ValueError:
             out = {'status': 'ERROR'}
-
         return out
 
     def download(ID: int, path: str) -> None:
@@ -56,40 +43,24 @@ def runMMseqs2(
         with open(path, 'wb') as out:
             out.write(res.content)
 
-    # Make input sequence a list if not already.
-    sequences = [sequences] if isinstance(sequences, str) else sequences
-
-    # Set the mode for MMseqs2.
-    if use_filter:
-        mode = 'env' if use_env else 'all'
-    else:
-        mode = 'env-nofilter' if use_env else 'nofilter'
-
-    if use_pairing:
-        mode = "pairgreedy"
-        if use_env:
-            mode = mode + "-env"
+    # Set the mode for MMseqs2 (always use env databases).
+    mode = 'env' if use_filter else 'env-nofilter'
 
     # Set up output path.
     os.makedirs(out_path, exist_ok=True)
     tar_gz_file = os.path.join(out_path, 'out.tar.gz')
     N, REDO = 101, True
 
-    # Deduplicate and keep track of order.
-    unique_seqs = []
-    [unique_seqs.append(seq) for seq in sequences if seq not in unique_seqs]
-    Ms = [N + unique_seqs.index(seq) for seq in sequences]
-
     # Call MMseqs2 API.
     if not os.path.isfile(tar_gz_file):
         while REDO:
             # Resubmit job until it goes through
-            out = submit(seqs=unique_seqs, mode=mode, N=N)
+            out = submit(seq=sequence, mode=mode, N=N)
 
             while out['status'] in ['UNKNOWN', 'RATELIMIT']:
                 # Resubmit
-                time.sleep(10 + 5 * (len(unique_seqs) // 50) + random.randint(0, 5))
-                out = submit(seqs=unique_seqs, mode=mode, N=N)
+                time.sleep(10 + random.randint(0, 5))
+                out = submit(seq=sequence, mode=mode, N=N)
 
             if out['status'] == 'ERROR':
                 raise Exception('MMseqs2 API is giving errors. Please confirm '
@@ -103,7 +74,7 @@ def runMMseqs2(
             # Wait for job to finish
             ID = out['id']
             while out['status'] in ['UNKNOWN', 'RUNNING', 'PENDING']:
-                time.sleep(5 + 5 * (len(unique_seqs) // 50) + random.randint(0, 5))
+                time.sleep(5 + random.randint(0, 5))
                 out = status(ID)
 
             if out['status'] == 'COMPLETE':
@@ -118,98 +89,70 @@ def runMMseqs2(
         # Download results
         download(ID, tar_gz_file)
 
-    # Get and extract a list of .a3m files.
-    if use_pairing:
-        a3m_files = [os.path.join(out_path, 'pair.a3m')]
-    else:
-        a3m_files = [os.path.join(out_path, 'uniref.a3m')]
-    if use_env:
-        a3m_files.append(
-            os.path.join(out_path, 'bfd.mgnify30.metaeuk30.smag30.a3m'))
+    # Selectively extract only the two .a3m files we want.
+    a3m_names = ['uniref.a3m', 'bfd.mgnify30.metaeuk30.smag30.a3m']
+    a3m_files = [os.path.join(out_path, name) for name in a3m_names]
+
     if not os.path.isfile(a3m_files[0]):
         with tarfile.open(tar_gz_file) as tar_gz:
-            tar_gz.extractall(out_path)
+            for name in a3m_names:
+                member = tar_gz.getmember(name)
+                member.name = os.path.basename(member.name)
+                tar_gz.extract(member, path=out_path)
 
     # Gather .a3m lines.
-    a3m_lines = {}
+    a3m_lines = []
     for a3m_file in a3m_files:
-        update_M, M = True, None
         with open(a3m_file, 'r') as f:
             for line in f:
                 if len(line) > 0:
                     # Replace NULL values
                     if '\x00' in line:
                         line = line.replace('\x00', '')
-                        update_M = True
-                    if line.startswith('>') and update_M:
-                        M = int(line[1:].rstrip())
-                        update_M = False
-                        if M not in a3m_lines:
-                            a3m_lines[M] = []
-                    a3m_lines[M].append(line)
+                    a3m_lines.append(line)
 
-    # Return results.
-    return [''.join(a3m_lines[n]) for n in Ms]
+    return ''.join(a3m_lines)
 
 
 def main(args: argparse.Namespace):
     os.makedirs(args.alignment_dir, exist_ok=True)
 
-    tag_list = []
-    seq_list = []
-    for fasta_file in list_files_with_extensions(args.fasta_dir, (".fasta", ".fa")):
-        fasta_path = os.path.join(args.fasta_dir, fasta_file)
-        for record in SeqIO.parse(fasta_path, "fasta"):
-            tag_list.append(record.id)
-            seq_list.append(str(record.seq))
+    records = list(SeqIO.parse(args.fasta_file, "fasta"))
+    if len(records) != 1:
+        raise ValueError(f"Expected exactly 1 sequence in {args.fasta_file}, found {len(records)}.")
 
-    for tag, seq in zip(tag_list, seq_list):
-        tag_dir = os.path.join(args.alignment_dir, tag)
-        a3m_lines = runMMseqs2(
-            out_path=tag_dir,
-            sequences=seq,
-            use_env=args.use_env,
-            use_pairing=args.use_pairing
-        )
+    tag = records[0].id
+    seq = str(records[0].seq)
 
-        for f in list_files_with_extensions(tag_dir, ".a3m"):
-            fp = os.path.join(tag_dir, f)
-            fp_new = fp + "2"  # Rename to .a3m2 so OpenFold won't detect
-            os.rename(fp, fp_new)
+    tag_dir = os.path.join(args.alignment_dir, tag)
+    a3m = runMMseqs2(
+        out_path=tag_dir,
+        sequence=seq,
+    )
 
-        with open(os.path.join(tag_dir, 'mmseqs_msa.a3m'), 'w') as f:
-            f.writelines(a3m_lines)
+    with open(os.path.join(tag_dir, 'mmseqs_msa.a3m'), 'w') as f:
+        f.write(a3m)
 
-    print(f'Computed MSAs for tags: {tag_list}')
-    print(f'Use "{args.alignment_dir}" for the "use_precomputed_alignments" flag.')
+    print(f'Computed MSA for: {tag}')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Fetch MSAs for protein sequences using the MMseqs2/ColabFold API.",
+        description="Fetch an MSA for a protein sequence using the MMseqs2/ColabFold API.",
         formatter_class=argparse.RawDescriptionHelpFormatter,
         epilog="""
 Examples:
-  python get_mmseqs2_msa.py ./fastas
-  python get_mmseqs2_msa.py ./fastas --alignment_dir ./alignments
-  python get_mmseqs2_msa.py ./fastas --alignment_dir ./alignments --use_pairing
+  python get_mmseqs2_msa.py my_protein.fasta
+  python get_mmseqs2_msa.py my_protein.fasta --alignment_dir ./alignments
         """,
     )
     parser.add_argument(
-        "fasta_dir", type=str,
-        help="Path to directory containing FASTA files"
+        "fasta_file", type=str,
+        help="Path to a FASTA file containing a single protein sequence"
     )
     parser.add_argument(
         "--alignment_dir", type=str, default=os.path.join(os.getcwd(), "alignments"),
-        help="Name of the directory in which to output the computed MSAs. Defaults to './alignments'"
-    )
-    parser.add_argument(
-        "--use_env", action='store_true', default=True,
-        help="Include the environmental sequence databases in search. Defaults to True"
-    )
-    parser.add_argument(
-        "--use_pairing", action='store_true', default=False,
-        help="Attempt to pair sequences from same species. Defaults to False."
+        help="Directory to write the computed MSA. Defaults to './alignments'"
     )
     args = parser.parse_args()
 
